64383d9e69fb555be53b4f0c320ef510f130d531
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-05-21 22:45:24 +1000

    Inheritance and Polimorphism DONE ; TODO: Write to file

--- Inheritance/Academy/Human.cs
+++ Inheritance/Academy/Human.cs
@@ -35,9 +35,15 @@ namespace Academy
 		{
 			Console.WriteLine($"HDestructor:\t{this.GetHashCode()}");
 		}
-		public void Info()
+		public virtual void Info()
 		{
 			Console.WriteLine($"{LastName} {FirstName} {Age}");
+		}
+
+
+		public override string ToString()
+		{
+			return base.ToString() + $":\t{LastName} {FirstName} {Age}";
 		}
 	}
 }
