f7a4899ae9e673830e2c613d8ad21ef2d03e927b
User <User@DESKTOP-QHG18FL>
2025-05-30 16:48:19 +0300

    ForwardList in progress

---
+++ DataContainers/FactorialChecked/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialChecked
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите число: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			long f = 1;
+			checked
+			{
+				try
+				{
+					for (int i = 1; i <= n; i++)
+					{
+						f *= i;
+						Console.WriteLine($"{i}! = {f};");
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				} 
+			}
+		}
+	}
+}
+//Decimal
